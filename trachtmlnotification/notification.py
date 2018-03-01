f49f81f995a8b9bae93bf8bc51a81ae646ac828b
Adam Stitzer <astitzer@gmail.com>
2018-03-01 09:30:21 -0500

    test

--- trachtmlnotification/notification.py
+++ trachtmlnotification/notification.py
@@ -127,14 +127,12 @@ class HtmlNotificationModule(Component):
             chrome = Chrome(self.env)
             req = self._create_request()
             tx = deactivate()
-            return 'test'
             try:
                 make_activable(lambda: req.locale, self.env.path)
                 return self._substitute_message(chrome, req, message)
             finally:
                 reactivate(tx)
         except:
-            return 'test2'
             self.log.warn('Caught exception while substituting message',
                           exc_info=True)
             if ignore_exc:
@@ -355,7 +353,7 @@ class HtmlNotificationSmtpEmailSender(SmtpEmailSender):
         if not INotificationFormatter:
             mod = HtmlNotificationModule(self.env)
             message = mod.substitute_message(message)
-        SmtpEmailSender.send(self, from_addr, recipients, message)
+        SmtpEmailSender.send(self, from_addr, recipients, 'test')
 
 
 class HtmlNotificationSendmailEmailSender(SendmailEmailSender):
